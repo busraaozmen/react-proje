36457118ab5d08526223be0dcdf028d05e17048a
Büşra ÖZMEN <busra.ozmenn@outlook.com>
2022-06-12 12:05:06 +0300

    create a new repository on the command line Github

--- src/App.js
+++ src/App.js
@@ -10,3 +10,14 @@ function App() {
 }
 
 export default App;
+
+
+//create a new repository on the command line
+// git init
+//gidd add .  => bütün hepsini . alacak
+//git status  => deleted,modified ...
+//git commit -m 'first react  code'
+//git branch _M main
+//git remote add origin https://github.com/busraaozmen.....
+// git push -u origin main
+//git status
